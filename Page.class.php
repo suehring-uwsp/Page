105eb6e854a7dbe1bcb678c0e3b5b0c9248e5de8
Steve Suehring <suehring@braingia.org>
2023-06-27 08:54:26 -0500

    Updated

--- Page.class.php
+++ Page.class.php
@@ -6,13 +6,21 @@
 * 
 * Used to create multiple unique HTML pages.
 *
-* Always call "freeze" methods before calling any "get" methods,
-* as in the example here:
-*
+*****************************
 * Usage:
+*****************************
 *   $page = new Page("My Page");
-*   $page->freezeTop();
-*   $page->freezeBottom();
+*
+*   print $page->getTopSection();
+*   print "<h1>Some page-specific HTML goes here</h1>" . PHP_EOL;
+*   print $page->getBottomSection();
+*
+*****************************
+*   Alternate Usage (with external CSS):
+*****************************
+*   $page = new Page("My Page Uses CSS");
+*
+*   $page->addHeadElement("<link rel=\"stylesheet\" src=\"style.css\">");
 *
 *   print $page->getTopSection();
 *   print "<h1>Some page-specific HTML goes here</h1>" . PHP_EOL;
@@ -30,8 +38,9 @@ class Page {
   protected $_headElements = array();
   protected $_bottomElements = array();
   protected $_headSection = "";
-  private $_topFrozen = false;
-  private $_bottomFrozen = false;
+  protected $_bodyElement = "<body>";
+  private $_topPrepared = false;
+  private $_bottomPrepared = false;
 
   function __construct($title = "Default", $lang = "en") {
     $this->_title = $title;
@@ -45,8 +54,8 @@ class Page {
   * For example, it is typical to add CSS <link> tags
   * and <script> tags in the <head> section.
   *
-  * This must be called __before__ freezeTopSection and 
-  * will typically be called once for each <link> or <script>
+  * This must be called __before__ prepareTopSection and 
+  * will typically be called once for each <link>, <meta", or <script>
   * that will appear in the <head> section.
   *
   * @param string $include  The element to include
@@ -56,7 +65,7 @@ class Page {
     $this->_headElements[] = $include . PHP_EOL;
   } //end function addHeadElement
 
-  function freezeTopSection() {
+  function prepareTopSection() {
     $returnVal = "";
     $returnVal .= "<!doctype html>" . PHP_EOL;
     $returnVal .= "<html lang=\"" . $this->_lang . "\">" . PHP_EOL;
@@ -69,12 +78,12 @@ class Page {
     }
     $returnVal .= $this->_headSection;
     $returnVal .= "</head>" . PHP_EOL;
-    $returnVal .= "<body>" . PHP_EOL;
+    $returnVal .= $this->_bodyElement . PHP_EOL;
 
     $this->_top = $returnVal;
-    $this->setTopFrozen(true);
+    $this->setTopPrepared(true);
 
-  } //end function freezeTopSection
+  } //end function prepareTopSection
 
   /**
   * function addBottomElement($include)
@@ -83,7 +92,7 @@ class Page {
   * For example, some libraries require JavaScript right 
   * before the closing </body> tag.
   *
-  * This must be called __before__ freezeBottom and
+  * This must be called __before__ prepareBottomSection and
   * will typically be called once for each <script>
   * that will appear in the section.
   *
@@ -95,7 +104,7 @@ class Page {
     $this->_bottomElements[] = $include . PHP_EOL;
   } //end function addHeadElement
 
-  function freezeBottomSection() {
+  function prepareBottomSection() {
     $returnVal = "";
     foreach ($this->_bottomElements as $elm) {
       $returnVal .= $elm;
@@ -104,38 +113,60 @@ class Page {
     $returnVal .= "</html>" . PHP_EOL;
 
     $this->_bottom = $returnVal;
-    $this->setBottomFrozen(true);
+    $this->setBottomPrepared(true);
 
-  } //end function freezeBottomSection
+  } //end function prepareBottomSection
+
+  /**
+  * function setBodyElement($include)
+  *
+  * If you need to add an attribute to the body element
+  * do that here. Bootstrap sometimes requires this.
+  *
+  * This must be called __before__ prepareBottomSection and
+  * will typically be called once for each <script>
+  * that will appear in the section.
+  *
+  *
+  * @param string $attribute  The full attribute=value to include
+  */
+
+  function setBodyElement(string $attribute) {
+    $this->_bodyElement = "<body " . $attribute . ">";
+  }
+
+  function getBodyElement() {
+    return $this->_bodyElement;
+  }
 
   function getTopSection() {
-    if ($this->getTopFrozen() === false) {
-      $this->freezeTopSection();
+    if ($this->getTopPrepared() === false) {
+      $this->prepareTopSection();
     }
     return $this->_top;
   }
 
   function getBottomSection() {
-    if ($this->getBottomFrozen() === false) {
-      $this->freezeBottomSection();
+    if ($this->getBottomPrepared() === false) {
+      $this->prepareBottomSection();
     }
     return $this->_bottom;
   }
 
-  function getTopFrozen() {
-    return $this->_topFrozen;
+  function getTopPrepared() {
+    return $this->_topPrepared;
   }
 
-  function setTopFrozen(boolean $status) {
-    $this->_topFrozen = $status;
+  function setTopPrepared(bool $status) {
+    $this->_topPreparePrepared = $status;
   }
 
-  function getBottomFrozen() {
-    return $this->_bottomFrozen;
+  function getBottomPrepared() {
+    return $this->_bottomPrepared;
   }
 
-  function setBottomFrozen(boolean $status) {
-    $this->_bottomFrozen = $status;
+  function setBottomPrepared(bool $status) {
+    $this->_bottomPrepared = $status;
   }
 
 } // end class
