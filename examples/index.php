75271fffaac012fef0f185a74f8d008aced6a23c
Steve Suehring <suehring@braingia.org>
2023-11-07 10:42:00 -0800

    fixing examples index

--- examples/index.php
+++ examples/index.php
@@ -2,7 +2,7 @@
 
 require_once(__DIR__ . "/../Page.php");
 
-$page = new Page("Page");
+$page = new NamespaceName\Page("Page");
 
 print $page->getTopSection();
 print "<h1>Page created with Page class</h1>" . PHP_EOL;
